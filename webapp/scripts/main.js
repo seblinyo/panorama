ba8ebe5fe847e1afecb7bbd8772834e67a151f7a
Sebastian Hancock <sebastian.hancock@tum.de>
2019-10-14 18:59:29 +0200

    playing around with mapbox

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,1 +1,11 @@
 console.log("Works");
+
+mapboxgl.accessToken =
+            'pk.eyJ1Ijoic2ViYXN0aWFuLWNoIiwiYSI6ImNpejkxdzZ5YzAxa2gyd21udGpmaGU0dTgifQ.IrEd_tvrl6MuypVNUGU5SQ';
+
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/dark-v9',
+            center: [13.673237, 47.402031], // starting position [lng, lat]
+            zoom: 14
+        });
