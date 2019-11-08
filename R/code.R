c7e5740e99809679a2de6b07eb7911bc6057f0d6
Sebastian Hancock <sebastian.hancock@tum.de>
2019-11-08 19:16:51 +0100

    update

--- R/code.R
+++ R/code.R
@@ -1,6 +1,6 @@
 library(rayshader)
 
-elev_img <- raster::raster("small.tif")
+elev_img <- raster::raster("new-cut.tif")
 #elev_matrix <- matrix(
 #raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
 #  nrow = ncol(elev_img), ncol = nrow(elev_img)
@@ -13,7 +13,7 @@ ambmat <- ambient_shade(elev_matrix, zscale = 30)
 raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
 watermap <- detect_water(elev_matrix)
 
-zscale <- 50
+zscale <- 40
 rgl::clear3d()
 elev_matrix %>% 
   sphere_shade(texture = "imhof1") %>% 
@@ -22,5 +22,5 @@ elev_matrix %>%
   add_shadow(ambmat, max_darken = 0.5) %>%
   plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
           water = TRUE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
-          theta = 70, phi = 30, zoom = 0.65, fov = 10)
+          theta = 250, phi = 20, zoom = 0.35, fov = 10)
 render_snapshot()
