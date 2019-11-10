7690f00d02e16bba0f124d28421d70bc343153c2
Sebastian Hancock <sebastian.hancock@tum.de>
2019-11-10 19:24:23 +0100

    update

--- R/geoviz-ex.R
+++ R/geoviz-ex.R
@@ -5,11 +5,10 @@ library(geoviz)
 lat <- 47.394319
 long <- 13.687770
 
-square_km <- 5
+square_km <- 6
 
-
-max_tiles <- 25
-mapbox_key <- "pk.eyJ1Ijoic2ViYXN0aWFuLWNoIiwiYSI6ImNpejkxdzZ5YzAxa2gyd21udGpmaGU0dTgifQ.IrEd_tvrl6MuypVNUGU5SQ"
+max_tiles <- 60
+#mapbox_key <- "pk.eyJ1Ijoic2ViYXN0aWFuLWNoIiwiYSI6ImNpejkxdzZ5YzAxa2gyd21udGpmaGU0dTgifQ.IrEd_tvrl6MuypVNUGU5SQ"
 
 
 
@@ -21,20 +20,37 @@ elmat = matrix(
   ncol = nrow(dem)
 )
 
-sunangle <- 60
+sunangle <- 270
 
-overlay_image <-
-  slippy_overlay(
-    dem,
-    image_source = "mapbox",
-    image_type = "satellite",
-    png_opacity = 0.9,
-    api_key = mapbox_key
-  )
+
+
+#overlay_image <-
+#  slippy_overlay(
+#    dem,
+#    image_source = "mapbox",
+#    image_type = "satellite",
+#    png_opacity = 0.6,
+#    api_key = mapbox_key
+# )
+
+
+#Optionally, turn mountainous parts of the overlay transparent
 
 scene <- elmat %>%
-  sphere_shade(sunangle = sunangle, texture = "bw") %>%
-  add_overlay(overlay_image)
+  sphere_shade(sunangle = sunangle, texture = "imhof1") %>%
+  #add_overlay(overlay_image) %>%
+  add_shadow(
+    ray_shade(
+      elmat,
+      anglebreaks = seq(30, 60),
+      sunangle = sunangle,
+      multicore = TRUE,
+      lambert = FALSE,
+      remove_edges = FALSE
+    )
+  ) %>%
+  add_shadow(ambient_shade(elmat, multicore = TRUE, remove_edges = FALSE))
+
 
 rayshader::plot_3d(
   scene,
@@ -44,4 +60,18 @@ rayshader::plot_3d(
   shadow = TRUE,
   shadowdepth = -150
 )
+
+
+
+rgl::view3d(theta =290, phi = 18, zoom = 0.5, fov = 5)
+
+
+
+#rayshader::render_depth(
+#  focus = 0.5,
+#  fstop = 18,
+#  filename = "scene.png"
+#)
+
+
 
